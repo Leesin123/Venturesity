d547de8a8d5e7e8f16f83c72d3fccf3fbbc0c49f
Harsha S Deshpande <harshasdeshpande@Harshas-MacBook-Pro.local>
2015-10-07 15:38:15 +0530

    New

--- permute.c
+++ permute.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **permutes;
+int indexi = 0;
 void swap(char *a,char *b){
 	char temp = *a;
 	*a = *b;
@@ -8,7 +13,12 @@ void swap(char *a,char *b){
 void permute( char *str,int s,int e){
 	int i;
      if(s == e){
-     	 printf("%s\n",str);
+     	
+          permutes[indexi] = (char*)malloc(e+1);
+          strcpy(permutes[indexi],str);
+          
+
+          indexi++;
      	 return;
  	  }
  	  for(i = s;i <= e;i++){
@@ -17,13 +27,48 @@ void permute( char *str,int s,int e){
  	  	swap(str+s,str+i);
  	  }
 }
+void sort(int n){
+	int i,j;
+	char temp[7];
+	for(i = 0;i < n-1;i++){
+		for(j = 0;j < n -1;j++){
+			if(strcmp(permutes[j],permutes[j+1]) > 0){
+				strcpy(temp,permutes[j]);
+				strcpy(permutes[j],permutes[j+1]);
+				strcpy(permutes[j+1],temp);
+			}
+		}
+	}
+}
 int main(){
 	char str[7];
-	int i=0;
+	int i=0,n,prod = 1,flag = 0;
 	printf("\nEnter the string\n");
 	scanf("%s",str);
-	while(str[i] != '\0')
-		i++;
-    permute(str,0,i-1);
+	
+	while(str[i] != '\0'){
+		    if(str[i] < 'A' || ((str[i] >'Z') && (str[i] < 'a')) || str[i] > 'z'){
+                flag = 1;
+                break;
+             }
+             
+              i++;
+
+    }
+    if(flag || i <= 2 || i > 6){
+    	printf("Invalid Input\n");
+    	exit(0);
+    }
+	n = i;
+    for(;i > 0;i--){
+    	prod *= i; 
+    } 
+	
+	printf("%d",prod);
+	permutes = (char**)malloc(prod*sizeof(char*));
+    permute(str,0,n-1);
+    sort(prod);
+    for(i = 0;i < prod;i++)
+    	 printf("%s\n",permutes[i]);
     return 0;
 }
